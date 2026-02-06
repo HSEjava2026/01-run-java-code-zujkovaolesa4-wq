c40444b50710ec5364d0dfe351bb7cd1a044cfc8
Олеся Жуйкова <C:\The Bat!>
2026-02-06 20:02:37 +0300

    2 Задание

---
+++ src/ru/hse/lab1/Main.java
@@ -0,0 +1,12 @@
+package ru.hse.lab1;
+
+import ru.hse.lab1.second.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 20);
+
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+    }
+}
---
+++ src/ru/hse/lab1/second/Person.java
@@ -0,0 +1,21 @@
+package ru.hse.lab1.second;
+
+public class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
+
